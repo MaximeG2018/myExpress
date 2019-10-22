eed86e114bbe6f8450456f294e2c80f2c43899e1
MaximeG2018 <maxime.gouenard@gmail.com>
2019-10-22 20:16:26 +0200

    Template

--- app.ts
+++ app.ts
@@ -1,32 +1,33 @@
 import myExpress from "./myExpress";
-import { IncomingMessage, ServerResponse } from "http"
+//import { IncomingMessage, ServerResponse } from "http"
+import * as http from 'http';
 
 const app = myExpress();
 const port = 3000;
 
 // GET
-app.get("/get",(req: IncomingMessage, res: ServerResponse) => {
+app.get("/get",(req: http.IncomingMessage, res: http.ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Je suis la Route : GET');
     res.end();
 })
 
 // DELETE
-app.delete("/delete",(req: IncomingMessage, res: ServerResponse) => {
+app.delete("/delete",(req: http.IncomingMessage, res: http.ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Je suis la Route : DELETE');
     res.end();
 })
 
 // POST
-app.post("/post", (req: IncomingMessage, res: ServerResponse) => {
+app.post("/post", (req: http.IncomingMessage, res: http.ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Je suis la Route : POST')
     res.end()
 })
 
 // PUT
-app.post("/put", (req: IncomingMessage, res: ServerResponse) => {
+app.post("/put", (req: http.IncomingMessage, res: http.ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('Je suis la Route : PUT')
     res.end()
@@ -34,7 +35,7 @@ app.post("/put", (req: IncomingMessage, res: ServerResponse) => {
 })
 
 // ALL
-app.all("/all", (req: IncomingMessage, res: ServerResponse) => {
+app.all("/all", (req: http.IncomingMessage, res: http.ServerResponse) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.write('ALL')
     res.end()
--- myExpress.ts
+++ myExpress.ts
@@ -1,59 +1,102 @@
-import { CreateServer, IncomingMessage, ServerResponse } from 'http';
+//import { CreateServer, IncomingMessage, ServerResponse } from 'http';
+import * as http from 'http';
+import * as fs from 'fs';
 
-interface Route {
-    method: string,
-    path: string,
-    callback: Function
+export interface Route {
+  method: string,
+  path: string,
+  callback: Function
 };
+
+export interface MyHttpResponse {
+  json: (item: any) => void
+  send: (content: string) => void
+};
+​
 
 class myExpress {
 
-    private routes: Route[] = [];
-    private httpServer: (http.Server | null) = null;
+  // Init Data
+  private routes: Route[] = [];
+  private server: any;
 
-    // GET
-    get(path: string, callback: Function): void {
-        this.routes.push({ method: "GET", path, callback });
-    }
+  // Constructor
+  constructor() {
+    this._initialize();
+  }
 
-    // DELETE
-    delete(path: string, callback: Function): void {
-        this.routes.push({ method: "DELETE", path, callback });
-    }
-
-    // PUT
-    put(path: string, callback: Function): void {
-        this.routes.push({ method: "PUT", path, callback });
-    }
-
-    //POST
-    post(path: string, callback: Function): void {
-        this.routes.push({ method: "POST", path, callback });
-    }
-
-    //ALL
-    all(path: string, callback: Function): void {
-        this.routes.push({method: "ALL", path, callback})
-    }
-
-    listen(port: number): void {
-        this.httpServer = CreateServer((req: IncomingMessage, res: ServerResponse) => {
-            const { method, url } = req;
-            const requestedRoute = this.routes.find((route): (Route | undefined) => {
-                if (route.method === method && route.path === url) {
-                    return route;
-                }
-                return undefined;
-            });
-            if (requestedRoute) {
-                requestedRoute.callback(req, res)
-                }
-            else {
-                res.SendStatus(4OO)
-                res.end();
-            }
-          })
+  // Method Private
+  private _initialize() {
+    this.server = http.CreateServer((req: http.IncomingMessage, res: http.ServerResponse) => {
+      const { method, url } = req;
+      const requestedRoute = this.routes.find((route): (Route | undefined) => {
+        if (route.method === method && route.path === url) {
+          return route;
+        }
+        return undefined;
+      });
+      if (requestedRoute) {
+        requestedRoute.callback(req, res);
       }
+      else {
+        res.status(400).send('Bad Request')
+        res.end();
+      }
+    })
+  }
+
+  private _overrideReponse(res: http.ServerResponse): MyHttpResponse {
+    const json = (item: any): void => {
+      res.write(JSON.stringify(item));
+      res.end();
+    }
+
+    const send = (content: string): void => {
+      res.write(content);
+      res.end();
+    }
+
+    return { ...res, json, send }
+  }
+
+  private _upper(str: string): string {
+    return str.toUpperCase()
+  }
+
+  private _lower(str: string): string {
+    return str.toLowerCase()
+  }
+
+  // Routes HTTP
+  // GET
+  get(path: string, callback: Function): void {
+    this.routes.push({ method: "GET", path, callback });
+  }
+
+  // DELETE
+  delete(path: string, callback: Function): void {
+    this.routes.push({ method: "DELETE", path, callback });
+  }
+
+  // PUT
+  put(path: string, callback: Function): void {
+    this.routes.push({ method: "PUT", path, callback });
+  }
+
+  // POST
+  post(path: string, callback: Function): void {
+    this.routes.push({ method: "POST", path, callback });
+  }
+
+  // ALL
+  all(path: string, callback: Function): void {
+    this.routes.push({ method: "ALL", path, callback })
+  }
+
+  // Listen
+  listen(port: number, callback: () => void): void {
+    this.server.listen(port, callback)
+  }
 }
 
 export default () => new myExpress();
